03f75fae5e955bd3925283326d72a6d3fe199d63
Gilbert40aar <60772847+Gilbert40aar@users.noreply.github.com>
2021-09-20 08:04:28 +0200

    TecTalk V3 designs
    Here I am uploading the complete design of TecTalk.

--- tectalkv3/src/app/app-routing.module.ts
+++ tectalkv3/src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { RecoverpwComponent } from './recoverpw/recoverpw.component';
+import { ChatComponent } from './chat/chat.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', pathMatch: 'full', redirectTo: 'login'},
+  {path: 'login', component: LoginComponent},
+  {path: 'register', component: RegisterComponent},
+  {path: 'recoverpw', component: RecoverpwComponent},
+  {path: 'chat', component: ChatComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- tectalkv3/src/app/app.module.ts
+++ tectalkv3/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { RecoverpwComponent } from './recoverpw/recoverpw.component';
+import { ChatComponent } from './chat/chat.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegisterComponent,
+    RecoverpwComponent,
+    ChatComponent
   ],
   imports: [
     BrowserModule,
